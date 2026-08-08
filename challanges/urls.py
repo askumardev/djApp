c9bc3fa9b6370fc0485a260c74f8e1ada0a4bb09
askumardev <askumar.dev@outlook.com>
2026-08-08 10:29:35 +0000

    monthly challange

--- challanges/urls.py
+++ challanges/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('january/', views.index, name='january'),
+    # path('january/', views.january),
+    # path('any_month/', views.any_month),
+    path('<int:month>', views.monthly_challenge_by_number),
+    path('<str:month>', views.monthly_challenge)
 ]
--- challanges/views.py
+++ challanges/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello World...")
+monthly_challenges = {
+    "january": "Hello January...",
+    "february": "Hello February...",
+    "march": "Hello March...",
+    "april": "Hello April...",
+    "may": "Hello May...",
+    "june": "Hello June...",
+    "july": "Hello July...",
+    "august": "Hello August...",
+    "september": "Hello September...",
+    "october": "Hello October...",
+    "november": "Hello November...",
+    "december": "Hello December..."
+}
 
+# def january(request):
+#     return HttpResponse("Hello January...")
+
+# def any_month(request):
+#     return HttpResponse("Hello Any Month...")
+
+def monthly_challenge(request, month):
+    try:
+        text = monthly_challenges[month]
+    except:
+        return HttpResponseNotFound("This month is not supported...")
+    return HttpResponse(text)
+
+def monthly_challenge_by_number(request, month):
+    return HttpResponse(month)
