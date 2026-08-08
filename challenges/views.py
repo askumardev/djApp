588ce16fc6f0f955d2da208fbb5e9b90bb41d10b
askumardev <askumar.dev@outlook.com>
2026-08-08 14:13:35 +0000

    view update

--- challenges/views.py
+++ challenges/views.py
@@ -5,18 +5,18 @@ from django.urls import reverse
 
 # Create your views here.
 monthly_challenges = {
-    "january": "Hello January...",
-    "february": "Hello February...",
-    "march": "Hello March...",
-    "april": "Hello April...",
-    "may": "Hello May...",
-    "june": "Hello June...",
-    "july": "Hello July...",
-    "august": "Hello August...",
-    "september": "Hello September...",
-    "october": "Hello October...",
-    "november": "Hello November...",
-    "december": "Hello December..."
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes every day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Eat no meat for the entire month!",
+    "may": "Walk for at least 20 minutes every day!",
+    "june": "Learn Django for at least 20 minutes every day!",
+    "july": "Eat no meat for the entire month!",
+    "august": "Walk for at least 20 minutes every day!",
+    "september": "Learn Django for at least 20 minutes every day!",
+    "october": "Eat no meat for the entire month!",
+    "november": "Walk for at least 20 minutes every day!",
+    "december": "Learn Django for at least 20 minutes every day!"
 }
 
 # def january(request):
